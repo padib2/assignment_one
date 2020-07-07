bfcee05e72713632ecf6616102e009ed6d097284
padib2 <67868866+padib2@users.noreply.github.com>
2020-07-07 19:49:51 -0400

    Add files via upload

---
+++ summation.py
@@ -0,0 +1,6 @@
+print("This program returns the sum of 2 integers")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+print("The sum of", a, "and", b, "is", a+b)
+
