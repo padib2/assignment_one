8ac708ec4a8b516f09f8ae5fc88c69f56c94014b
padib2 <67868866+padib2@users.noreply.github.com>
2020-07-08 14:11:45 -0400

    Delete producter.py

--- producter.py
+++
@@ -1,6 +0,0 @@
-print("This program takes two numbers as input and returns the product")
-a = int(input("Enter the first number: "))
-b = int(input("Enter the next number: "))
-product = a*b
-print("The product of",a,"and",b,"is",product)
-
