10bcdaaafbb383007e4f020541c31c1a8e3e98ae
padib2 <67868866+padib2@users.noreply.github.com>
2020-07-07 22:27:39 -0400

    Add files via upload

---
+++ producter.py
@@ -0,0 +1,6 @@
+print("This program takes two numbers as input and returns the product")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the next number: "))
+product = a*b
+print("The product of",a,"and",b,"is",product)
+
